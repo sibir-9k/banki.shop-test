3ee69267037fd4790afc34c783d0139859cf68b9
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-04-03 22:27:33 +0300

    states have been added for card search and basic functionality has been added for this

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import './App.css'
+import { useState, useEffect } from 'react'
+import data from './data/data.js'
 import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
+import './App.css'
 
 function App() {
+  const [search, setSearch] = useState('')
+  const [cardList, setCardList] = useState(data)
+
+  const filterCards = (searchText, listOfCards) => {
+    if (!searchText) {
+      return listOfCards;
+    }
+    return listOfCards.filter((card) =>
+      card.title.toLowerCase().includes(searchText.toLowerCase())
+    );
+  }
+
+  useEffect(() => {
+    const debounce = setTimeout(() => {
+      const filteredCards = filterCards(search, data);
+      setCardList(filteredCards);
+    }, 150);
+    return () => clearTimeout(debounce);
+  }, [search]);
+
+
   return (
     <div className="App">
-      <Header />
-      <Main />
+      <Header value={search} setValue={setSearch} />
+      <Main cards={cardList} />
       <Footer />
     </div>
   )
