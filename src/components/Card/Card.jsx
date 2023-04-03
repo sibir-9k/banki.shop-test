fc6fad9ae91fa2a97368b4b1fe7a7c9c22051f4a
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-04-03 22:17:59 +0300

    a card component with basic functionality has been created

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,30 +1,52 @@
-import React from 'react'
-
-import './Card.scss'
+import React, { useState } from 'react';
+import { MdDone } from 'react-icons/md'
+import './Card.scss';
+import { Loader } from '../Loader/Loader';
 
 export const Card = ({ card }) => {
-  const { title, imgUrl, newPrice, oldPrice } = card
+  const { id, title, imgUrl, newPrice, oldPrice } = card;
+
+  const [loading, setLoading] = useState(false)
+  const [sold, setSold] = useState(false)
+
+  const buyingProduct = () => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+      setSold(true)
+    }, 2000)
+  }
+
   return (
-    <div className="card">
+    <div className={id !== 4 ? 'card' : 'card opacity'}>
       <div className="card-img">
         <img src={imgUrl} alt="" />
       </div>
       <div className="card-info">
         <div className="card-info__productName">
-          <div to={'product'} >{title}</div>
+          <div to={'product'}>{title}</div>
         </div>
         <div className="card-info__buy-price">
-          {(oldPrice || newPrice) 
-          ? <div className='price'>
+          {oldPrice || newPrice ? (
+            <div className="price">
               {oldPrice ? <span className="price-old">{oldPrice} $</span> : ''}
               <span className="price-new">{newPrice} $</span>
-            </div> 
-          : ''}
-          <button className="buy">
-            Купить
-          </button>
+            </div>
+          ) : (
+            ''
+          )}
+          {
+            loading
+              ? <div style={{ display: 'flex', justifyContent: 'center', marginLeft: '40px' }}><Loader /></div>
+              : oldPrice || newPrice ? (
+                <button className="buy" onClick={buyingProduct}>{sold ? <span style={{ display: 'flex', alignItems: 'center' }}><MdDone />В корзине</span> : 'Купить'}</button>
+              ) : (
+                <span className="sold">Продана на аукционе</span>
+              )
+          }
+
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
