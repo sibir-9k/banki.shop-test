409dce34b0208ed9232d018169a1b2f425a6979e
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-04-03 22:23:46 +0300

    added new components Loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BiLoader } from 'react-icons/bi';
+import './Loader.scss'
+
+export const Loader = () => {
+  return (
+    <div className='loader'>
+      <BiLoader />
+    </div>
+  )
+}
