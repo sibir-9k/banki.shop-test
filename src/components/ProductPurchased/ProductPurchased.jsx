6a2cbd97740dde6934c828c4694e0bc32b36d3ad
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-04-04 00:54:51 +0300

    put some jsx into a new component

---
+++ src/components/ProductPurchased/ProductPurchased.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { MdDone } from 'react-icons/md'
+
+export const ProductPurchased = () => {
+  return (
+    <span style={{ display: 'flex', alignItems: 'center' }}>
+      <MdDone />В корзине
+    </span>
+  )
+}
