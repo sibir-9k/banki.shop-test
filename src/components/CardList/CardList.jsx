5a05207f7821820d1fad813d1cab8232376ff5f4
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-04-03 22:20:38 +0300

    the Card List component was created to display cards

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Card } from '../Card/Card.jsx'
-import data from '../../data/data'
+// import data from '../../data/data'
 
 import './CardList.scss'
 
-export const CardList = () => {
+export const CardList = ({ cards }) => {
   return (
     <div className='card-list'>
       <ul>
-        {data.map(card => {
-          return <Card key={card.id} card={card} />
+        {cards.map(card => {
+          return (
+            <li key={card.id} >
+              <Card card={card} />
+            </li>
+          )
         })}
       </ul>
     </div>
