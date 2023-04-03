a74f3b6c2b085c15aedee80cd8903fb01cd9fe06
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-04-04 00:55:39 +0300

    put some jsx into a new component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,52 +1,52 @@
 import React, { useState } from 'react';
-import { MdDone } from 'react-icons/md'
-import './Card.scss';
 import { Loader } from '../Loader/Loader';
+import { ProductPurchased } from '../ProductPurchased/ProductPurchased';
+import './Card.scss';
 
 export const Card = ({ card }) => {
-  const { id, title, imgUrl, newPrice, oldPrice } = card;
+	const { id, title, imgUrl, newPrice, oldPrice } = card;
+	const [loading, setLoading] = useState(false);
+	const [sold, setSold] = useState(false);
 
-  const [loading, setLoading] = useState(false)
-  const [sold, setSold] = useState(false)
+	const buyingProduct = () => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+			setSold(true);
+		}, 2000);
+	};
 
-  const buyingProduct = () => {
-    setLoading(true)
-    setTimeout(() => {
-      setLoading(false)
-      setSold(true)
-    }, 2000)
-  }
-
-  return (
-    <div className={id !== 4 ? 'card' : 'card opacity'}>
-      <div className="card-img">
-        <img src={imgUrl} alt="" />
-      </div>
-      <div className="card-info">
-        <div className="card-info__productName">
-          <div to={'product'}>{title}</div>
-        </div>
-        <div className="card-info__buy-price">
-          {oldPrice || newPrice ? (
-            <div className="price">
-              {oldPrice ? <span className="price-old">{oldPrice} $</span> : ''}
-              <span className="price-new">{newPrice} $</span>
-            </div>
-          ) : (
-            ''
-          )}
-          {
-            loading
-              ? <div style={{ display: 'flex', justifyContent: 'center', marginLeft: '40px' }}><Loader /></div>
-              : oldPrice || newPrice ? (
-                <button className="buy" onClick={buyingProduct}>{sold ? <span style={{ display: 'flex', alignItems: 'center' }}><MdDone />В корзине</span> : 'Купить'}</button>
-              ) : (
-                <span className="sold">Продана на аукционе</span>
-              )
-          }
-
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={id !== 4 ? 'card' : 'card opacity'}>
+			<div className="card-img">
+				<img src={imgUrl} alt="" />
+			</div>
+			<div className="card-info">
+				<div className="card-info__productName">
+					<div to={'product'}>{title}</div>
+				</div>
+				<div className="card-info__buy-price">
+					{oldPrice || newPrice ? (
+						<div className="price">
+							{oldPrice ? <span className="price-old">{oldPrice} $</span> : ''}
+							<span className="price-new">{newPrice} $</span>
+						</div>
+					) : (
+						''
+					)}
+					{loading ? (
+						<div style={{ display: 'flex', justifyContent: 'center', marginLeft: '40px' }}>
+							<Loader />
+						</div>
+					) : oldPrice || newPrice ? (
+						<button className="buy" onClick={buyingProduct}>
+							{sold ? <ProductPurchased /> : 'Купить'}
+						</button>
+					) : (
+						<span className="sold">Продана на аукционе</span>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
