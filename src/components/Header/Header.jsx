79c9848fbadaeabdc63fda7d8eb943af91db3271
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-04-03 22:22:51 +0300

    Propulsions added for the input

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
 import './Header.scss'
 
-export const Header = () => {
+export const Header = ({ value, setValue }) => {
+
   return (
     <div className='header'>
       <nav className='header-nav'>
@@ -16,8 +16,12 @@ export const Header = () => {
       </nav>
       <div className='header-search'>
         <form>
-          <input type="text" placeholder="Поиск по названию картины"/>
-            <button type="submit">Найти</button>
+          <input
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            type="text"
+            placeholder="Поиск по названию картины" />
+          <button type="submit">Найти</button>
         </form>
       </div>
     </div>
