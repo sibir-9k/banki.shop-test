9a9ae361a1c3fe298f68e45b1781f0a5f81db8c0
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2023-04-04 17:18:21 +0300

    added ability to save status after browser page reloads

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,52 +1,59 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader/Loader';
 import { ProductPurchased } from '../ProductPurchased/ProductPurchased';
 import './Card.scss';
 
 export const Card = ({ card }) => {
-	const { id, title, imgUrl, newPrice, oldPrice } = card;
-	const [loading, setLoading] = useState(false);
-	const [sold, setSold] = useState(false);
+  const { id, title, imgUrl, newPrice, oldPrice } = card;
+  const [loading, setLoading] = useState(false);
+  const [sold, setSold] = useState(
+    JSON.parse(window.sessionStorage.getItem(`sold.${id}`)) || false
+  );
 
-	const buyingProduct = () => {
-		setLoading(true);
-		setTimeout(() => {
-			setLoading(false);
-			setSold(true);
-		}, 2000);
-	};
+  const buyingProduct = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setSold(true);
+    }, 2000);
+  };
 
-	return (
-		<div className={id !== 4 ? 'card' : 'card opacity'}>
-			<div className="card-img">
-				<img src={imgUrl} alt="" />
-			</div>
-			<div className="card-info">
-				<div className="card-info__productName">
-					<div to={'product'}>{title}</div>
-				</div>
-				<div className="card-info__buy-price">
-					{oldPrice || newPrice ? (
-						<div className="price">
-							{oldPrice ? <span className="price-old">{oldPrice} $</span> : ''}
-							<span className="price-new">{newPrice} $</span>
-						</div>
-					) : (
-						''
-					)}
-					{loading ? (
-						<div style={{ display: 'flex', justifyContent: 'center', marginLeft: '40px' }}>
-							<Loader />
-						</div>
-					) : oldPrice || newPrice ? (
-						<button className="buy" onClick={buyingProduct}>
-							{sold ? <ProductPurchased /> : 'Купить'}
-						</button>
-					) : (
-						<span className="sold">Продана на аукционе</span>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    sessionStorage.setItem(`sold.${id}`, JSON.stringify(sold))
+  }, [sold])
+
+
+  return (
+    <div className={id !== 4 ? 'card' : 'card opacity'}>
+      <div className="card-img">
+        <img src={imgUrl} alt="" />
+      </div>
+      <div className="card-info">
+        <div className="card-info__productName">
+          <div to={'product'}>{title}</div>
+        </div>
+        <div className="card-info__buy-price">
+          {oldPrice || newPrice ? (
+            <div className="price">
+              {oldPrice ? <span className="price-old">{oldPrice} $</span> : ''}
+              <span className="price-new">{newPrice} $</span>
+            </div>
+          ) : (
+            ''
+          )}
+          {loading ? (
+            <div style={{ display: 'flex', justifyContent: 'center', marginLeft: '40px' }}>
+              <Loader />
+            </div>
+          ) : oldPrice || newPrice ? (
+            <button className="buy" onClick={buyingProduct}>
+              {sold ? <ProductPurchased /> : 'Купить'}
+            </button>
+          ) : (
+            <span className="sold">Продана на аукционе</span>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
